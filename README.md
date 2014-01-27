faccf81dda71e27b6bcac7d45f9fc50b7197af0e
Hugo MACHEFER <hugo.machefer@gmail.com>
2014-01-27 17:38:46 +0100

    first commit: lets' backup everything

--- README.md
+++ README.md
@@ -1,4 +1,58 @@
 fps-meter-component
 ===================
 
-&lt;fps-meter> component helps adding FPSMeter graphics by a dedicated basic markup and its theme as a tag attribute
+
+TODO: This project aims to provide a box on-the-shelf with graphic display dedicated to FPS measurements
+
+> Maintained by [Hugo Machefer](https://github.com/hmachefe).
+
+## Demo
+
+> [Check it live](http://hugo.machefer.free.fr/fps-meter-component/).
+
+## Usage
+
+1. Import Web Components' polyfill:
+
+	```html
+	<script src="../lib/polymer.js"></script>
+	<script src="../lib/Platform.js"></script>	
+	```
+
+2. Import Custom Element:
+
+	```html
+    <link rel="import" href="src/fps-meter.html">
+	```
+
+3. Start using it!
+
+	```html
+    <fps-meter></fps-meter>
+    <fps-meter theme="light"></fps-meter>
+    <fps-meter theme="colorful"></fps-meter>
+    <fps-meter theme="transparent"></fps-meter>
+	```
+
+## Options
+
+Attribute  | Options                   | Default             | Description
+---        | ---                       | ---                 | ---
+`theme`    | *string*                     | `undefined`                 | skin that shall be used to display FPS graphic
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -m 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+* v0.0.1 Jan 1, 2014
+	* Started project using [polymer](https://github.com/polymer/polymer)
+
+## License
+
+[MIT License](http://opensource.org/licenses/MIT)
---
+++ src/fps-meter-use.js
@@ -0,0 +1,59 @@
+﻿	 var root = null;
+     window.application = {}
+     window.application.fpsmetersarray = [];                    /* some way to store fpsmeters components declared in HTML container :  */
+     FPSMeter = window.FPSMeter;                                /* uggly hack to retrieve FPSMeter from instantiation in fps-meter.js*/
+     window.application.FPSMeterPosition = 200;                 /* some way to store FPSMeterPosition */
+     var fpsbox = document.querySelector("#fpsbox");
+
+     FPSMeter.defaults.top = 0;
+     FPSMeter.defaults.left = 'auto';
+     FPSMeter.defaults.heat = 1;
+     FPSMeter.defaults.graph = 1;
+
+	 Polymer('fps-meter', {
+
+		 ready: function () {
+			 root = this.shadowRoot || this.webkitShadowRoot;
+			 this.theme = this.getAttribute("theme");           // "this.theme" may be considered as FPSMeter (Polymer) instance property, not FPSMeter (Polymer) class property.
+		 },                                                     // Meaning last them passed from <fps-meter theme="xxx"> declaration will become global to every FPSMeter instances
+
+         created: function () {
+             application.FPSMeterPosition += 200;
+             this.theme = this.getAttribute("theme");
+             var fpsm;
+             var marginValue = '0 ' + application.FPSMeterPosition + 'px' + ' 0 0';
+             if (this.theme != undefined) {
+                fpsm =  new FPSMeter(fpsbox, {
+                     right:     '0%',
+                     margin:    marginValue,
+                     theme:     this.theme
+                 });
+             }
+             else  {       /* this.theme == undefined */
+                 fpsm =  new FPSMeter(fpsbox, {
+                     right:     '0%',
+                     margin:    marginValue
+                 });
+             }
+             window.application.fpsmetersarray.push(fpsm);
+
+             function tick() {
+                 var meters = application.fpsmetersarray;
+                 setTimeout(tick, Math.max(Math.sin(+new Date() * 0.0015) * 50 + 50, 16));
+                 var i=0;
+                 for (;i<=(meters.length - 1); i++) {
+                     meters[i].tick();
+                 }
+             };
+
+             if (application.fpsmetersarray.length == 4) {      // assuming there are 4 <fps-meter...> tags declared in HTML page container
+                 tick();                                        // shall be fixed/improved later on
+             }                                                  // by detecting how many components are embedded in HTML container
+
+         },
+
+		 attributeChanged: function (attrName, oldVal, newVal) {
+			 this.theme = newVal;
+		 }
+
+	 });
